78c4505806e4efb629ca728f04c31c50ec4e142e
Sam Bauch <sbauch@gmail.com>
2021-02-08 11:46:26 -0500

    default imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 
-import { root } from './index.module.css'
+import styles from './index.module.css';
 
+const App = () => <h1 className={styles.root}>Hello!</h1>;
 
-const App = () => <h1 className={root}>Hello!</h1>;
-
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(<App />, document.getElementById('app'));
